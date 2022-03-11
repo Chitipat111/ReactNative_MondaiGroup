4c5f131024d23eac15ff3330a24702fd7d110984
Chitipat111 <87640233+Chitipat111@users.noreply.github.com>
2022-03-11 20:21:31 +0700

    R4_11/03/22

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
@@ -14,28 +15,47 @@ import SettingScreen from './screens/SettingScreen'
 import ChoosePicScreen from './screens/ChoosePicScreen'
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='LoginScreen' //รอเปลี่ยน
+      <Tab.Navigator initialRouteName='LessonScreen' //รอเปลี่ยน
       screenOptions={{
         headerStyle: { backgroundColor: '#202225', elevation: 0, shadowOpacity: 0, borderBottomWidth: 0},
         headerTintColor: '#fff',
         headerTitleStyle: { fontWeight: 'bold'},        
       }}>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="LessonScreen" component={LessonScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="LessonInfoScreen" component={LessonInfoScreen} options={{ title:'Lesson Infomation',headerTitleAlign: 'center'}}/>
-        <Stack.Screen name="QuestionScreen" component={QuestionScreen} options={{ title:'Question',headerTitleAlign: 'center'}}/>
-        <Stack.Screen name="ResultScreen" component={ResultScreen} options={{ title:'Question',headerTitleAlign: 'center'}}/>
-        <Stack.Screen name="ProfileScreen" component={ProfileScreen} options={{ title:'Profile',headerTitleAlign: 'center'}}/>
-        <Stack.Screen name="SettingScreen" component={SettingScreen} options={{ title:'SettingScreen',headerTitleAlign: 'center'}}/>  
-        <Stack.Screen name="ChoosePicScreen" component={ChoosePicScreen} options={{ title:'Choose Picture',headerTitleAlign: 'center'}}/>
-      </Stack.Navigator>
+        <Tab.Screen name="LoginScreen" component={LoginScreen} 
+        options={{ headerShown: false,tabBarButton: () => null,tabBarVisible: false, tabBarStyle: {display: 'none'} }}/>
+
+        <Tab.Screen name="RegisterScreen" component={RegisterScreen} 
+        options={{ headerShown: false,tabBarButton: () => null,tabBarVisible: false, tabBarStyle: {display: 'none'} }}/>
+
+        <Tab.Screen name="LessonScreen" component={LessonScreen} 
+        options={{ headerShown: false }}/>
+
+        <Tab.Screen name="LessonInfoScreen" component={LessonInfoScreen} 
+        options={{ title:'Lesson Infomation',headerTitleAlign: 'center',tabBarButton: () => null,tabBarVisible: false}}/>
+
+        <Tab.Screen name="QuestionScreen" component={QuestionScreen} 
+        options={{ title:'Question',headerTitleAlign: 'center',tabBarButton: () => null,tabBarVisible: false , tabBarStyle: {display: 'none'} }}/>
+
+        <Tab.Screen name="ResultScreen" component={ResultScreen} 
+        options={{ title:'Question',headerTitleAlign: 'center',tabBarButton: () => null,tabBarVisible: false , tabBarStyle: {display: 'none'} }}/>
+
+        <Tab.Screen name="ProfileScreen" component={ProfileScreen} 
+        options={{ title:'Profile',headerTitleAlign: 'center'}}/>
+        
+        <Tab.Screen name="SettingScreen" component={SettingScreen} 
+        options={{ title:'SettingScreen',headerTitleAlign: 'center'}}/>
+        
+        <Tab.Screen name="ChoosePicScreen" component={ChoosePicScreen} 
+        options={{ title:'Choose Picture',headerTitleAlign: 'center',tabBarButton: () => null,tabBarVisible: false , tabBarStyle: {display: 'none'} }}/>
+        
+      </Tab.Navigator>
     </NavigationContainer>
   )
-}
+}    
 
 export default App
