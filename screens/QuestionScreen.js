eb6e9609791fd84aeb48df03033ba7eebfd7bbd6
FFahPhuri <85545858+FFahPhuri@users.noreply.github.com>
2022-03-14 16:05:05 +0700

    R6 : 14/03/2022 Really FINISH

--- screens/QuestionScreen.js
+++ screens/QuestionScreen.js
@@ -8,36 +8,33 @@ import {
   SafeAreaView,
 } from 'react-native';
 
+const QuestionScreen = ({ navigation,route }) => {
+  const { lang } = route.params;
+  const {chapter} = route.params;
 
-const QuestionScreen = ({ navigation}) => {
-  // const QuestionScreen = ({ navigation,route }) => {
-  // const { name } = route.params;
-const lang = "HTML";
-const chapter = "ความรู้เบื้องต้นเกี่ยวกับ HTML";
+  const javafile = require('../question/JAVA.json');
+  const htmlfile = require('../question/HTML.json');
 
-const javafile = require('../question/JAVA.json')
-const htmlfile = require('../question/HTML.json')
+  const customData = lang == 'JAVA' ? javafile : htmlfile;
 
+  const correct = require('../assets/true.png');
+  const wrong = require('../assets/false.png');
 
-const customData = (lang=='JAVA'?javafile:htmlfile)
-
-
-  //const customData = require(filename);
-  // const [qusetion,setQusetion]=useState([]);
   const [number, setNumber] = useState(1);
-  const [score , setScore] = useState(0);
-  //())=>setChooseChoice(0)
+  const [score, setScore] = useState(0);
+
   const numQues = Object.keys(customData[chapter]).length;
   const [chooseChoice, setChooseChoice] = useState(0);
-  const fullQuestion =
-    customData[chapter][number.toString()];
+  const fullQuestion = customData[chapter][number.toString()];
   const quesType = fullQuestion['Q_Type'];
   const question = fullQuestion['Quiz'];
   const choice = fullQuestion['Choice'];
   const ans = fullQuestion['Ans'];
+
   //trueFalseQuiz
   const showOpacity = quesType === '1' ? 1 : 0;
   const choiceVisible = quesType === '1' ? false : true;
+
   //answer
   var quesColor; //same as next button
   if (chooseChoice === ans) {
@@ -50,15 +47,11 @@ const customData = (lang=='JAVA'?javafile:htmlfile)
   const chooseChoiceColor = chooseChoice === ans ? '#2ECD81' : '#EE4F4F';
   const notChooseChoiceColor = chooseChoice === 0 ? '#2F3136' : '#C4C4C4';
 
-  const nextOpacity = ((chooseChoice === 0 || number===5) ? 0 : 1);
-  const nextVisible = ((chooseChoice === 0 || number===5) ? true : false);
+  const nextOpacity = chooseChoice === 0 || number === 5 ? 0 : 1;
+  const nextVisible = chooseChoice === 0 || number === 5 ? true : false;
 
   const afterAns = chooseChoice === 0 ? false : true;
-
-  //const [quesType,setQuesType] = useState()
-  useEffect(() => {
-    // alert(filename)
-  });
+  const widthAfterAns = chooseChoice === 0 ? 270 : 250;
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: quesColor }}>
@@ -67,8 +60,13 @@ const customData = (lang=='JAVA'?javafile:htmlfile)
         <Text style={{ color: 'white', fontSize: 18 }}>
           {number}/{numQues}
         </Text>
-        <Text style={{ color: 'white', fontSize: 24, marginTop: 50 }}>
-  
+        <Text
+          style={{
+            color: 'white',
+            fontSize: 24,
+            marginTop: 50,
+            marginLeft: 10,
+          }}>
           {question}
           {/* {customData['Variable_&_Variable_type'][number.toString()]['Quiz']}*/}
         </Text>
@@ -76,14 +74,16 @@ const customData = (lang=='JAVA'?javafile:htmlfile)
 
       {/*Choice*/}
       <View style={{ flex: 1, backgroundColor: 'white', marginTop: 100 }}>
-        <View style={{ marginTop: 70, alignItems: 'center' }}>
+      {/*Choice1*/}
+        <View style={{ flexDirection: 'row', marginTop: 70 }}>
           <TouchableOpacity
             disabled={afterAns}
             style={{
+              marginLeft: 10,
               color: 'white',
               backgroundColor:
                 1 === chooseChoice ? chooseChoiceColor : notChooseChoiceColor,
-              width: 250,
+              width: widthAfterAns,
               height: 35,
               borderRadius: 7,
               justifyContent: 'center',
@@ -91,15 +91,23 @@ const customData = (lang=='JAVA'?javafile:htmlfile)
             }}
             onPress={() => setChooseChoice(1)}>
             <Text style={{ color: 'white', fontSize: 18 }}>A) {choice[0]}</Text>
-          </TouchableOpacity>
+          </TouchableOpacity>{' '}
+          <Image
+            source={1 === ans ? correct : wrong}
+            style={{ opacity: 1 === chooseChoice ? 1 : 0, marginLeft: 5}}
+          />
+        </View>
+        {/*Choice2*/}
+        <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity
             disabled={afterAns}
             style={{
+              marginLeft: 10,
               color: 'white',
               backgroundColor:
                 2 === chooseChoice ? chooseChoiceColor : notChooseChoiceColor,
               marginTop: 30,
-              width: 250,
+              width: widthAfterAns,
               height: 35,
               borderRadius: 7,
               justifyContent: 'center',
@@ -108,15 +116,23 @@ const customData = (lang=='JAVA'?javafile:htmlfile)
             onPress={() => setChooseChoice(2)}>
             <Text style={{ color: 'white', fontSize: 18 }}>B) {choice[1]}</Text>
           </TouchableOpacity>
+          <Image
+            source={2 === ans ? correct : wrong}
+            style={{ opacity: 2 === chooseChoice ? 1 : 0, marginLeft: 5,marginTop: 30}}
+          />
+        </View>
+        {/*Choice3*/}
+        <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity
             disabled={0 === chooseChoice ? choiceVisible : afterAns}
             style={{
+              marginLeft: 10,
               opacity: showOpacity,
               color: 'white',
               backgroundColor:
                 3 === chooseChoice ? chooseChoiceColor : notChooseChoiceColor,
               marginTop: 30,
-              width: 250,
+              width: widthAfterAns,
               height: 35,
               borderRadius: 7,
               justifyContent: 'center',
@@ -125,15 +141,23 @@ const customData = (lang=='JAVA'?javafile:htmlfile)
             onPress={() => setChooseChoice(3)}>
             <Text style={{ color: 'white', fontSize: 18 }}>C) {choice[2]}</Text>
           </TouchableOpacity>
+          <Image
+            source={3 === ans ? correct : wrong}
+            style={{ opacity: 3 === chooseChoice ? 1 : 0, marginLeft: 5,marginTop: 30}}
+          />
+        </View>
+        {/*Choice4*/}
+        <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity
             disabled={0 === chooseChoice ? choiceVisible : afterAns}
             style={{
+              marginLeft: 10,
               opacity: showOpacity,
               color: 'white',
               backgroundColor:
                 4 === chooseChoice ? chooseChoiceColor : notChooseChoiceColor,
               marginTop: 30,
-              width: 250,
+              width: widthAfterAns,
               height: 35,
               borderRadius: 7,
               justifyContent: 'center',
@@ -142,6 +166,10 @@ const customData = (lang=='JAVA'?javafile:htmlfile)
             onPress={() => setChooseChoice(4)}>
             <Text style={{ color: 'white', fontSize: 18 }}>D) {choice[3]}</Text>
           </TouchableOpacity>
+          <Image
+            source={4 === ans ? correct : wrong}
+            style={{ opacity: 4 === chooseChoice ? 1 : 0, marginLeft: 5,marginTop: 30 }}
+          />
         </View>
       </View>
 
@@ -167,13 +195,14 @@ const customData = (lang=='JAVA'?javafile:htmlfile)
           }}
           onPress={() => {
             setNumber(number + 1);
-            (ans===chooseChoice?setScore(score+1):setScore(score+0))
-            setChooseChoice(0);   
+            ans === chooseChoice ? setScore(score + 1) : setScore(score + 0);
+            setChooseChoice(0);
           }}>
           <Text style={{ color: 'white' }}>Next</Text>
         </TouchableOpacity>
-        </View>
-        <View
+      </View>
+      {/*Nextbtn end*/}
+      <View
         style={{
           flex: 0.105,
           backgroundColor: 'white',
@@ -181,9 +210,10 @@ const customData = (lang=='JAVA'?javafile:htmlfile)
           justifyContent: 'flex-end',
           padding: 20,
         }}>
-        <TouchableOpacity disabled={(number===5?false:true)}
+        <TouchableOpacity
+          disabled={number === 5 ? false : true}
           style={{
-            opacity : (number===5?1:0),
+            opacity: number === 5 ? 1 : 0,
             backgroundColor: quesColor,
             width: 100,
             height: 35,
@@ -192,7 +222,14 @@ const customData = (lang=='JAVA'?javafile:htmlfile)
             borderRadius: 7,
           }}
           onPress={() => {
-            {navigation.navigate('ResultScreen',{sendScore:score,maxScore:numQues,answer:ans,choice:chooseChoice})}
+            {
+              navigation.navigate('ResultScreen', {
+                sendScore: score,
+                maxScore: numQues,
+                answer: ans,
+                choice: chooseChoice,
+              });
+            }
           }}>
           <Text style={{ color: 'white' }}>Next</Text>
         </TouchableOpacity>
