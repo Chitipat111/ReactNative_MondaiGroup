90fa6449b6b1b87af5753da442cb3f1865c52d22
PlyNatwara <88375494+PlyNatwara@users.noreply.github.com>
2022-03-15 14:34:10 +0700

    Almost Final

--- context/UserContext.js
+++ context/UserContext.js
@@ -1,17 +1,23 @@
 import React from "react"
+import jwt_decode from "jwt-decode";
 
 export const UserStoreContext = React.createContext(); //Global
 
 const UserStoreProvider = ({children})=>{
     
     const [Token,setToken] = React.useState('nonToken');
-    // const [Token,setToken] = React.useState({Token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywidXNlcm5hbWUiOiJ0ZXN0ZXIiLCJwYXNzd29yZCI6IiQyYiQxMiRZY0NaNHpIdTJkVXc4czZ0Um1QazhlclNaVHNGa0lKeEY3eWxyQWt1UUNXOEJxZ1llLkhlTyIsImltYWdlIjoxLCJpYXQiOjE2NDY5OTc0NjcsImV4cCI6MTY0NzA4Mzg2N30.labGF63EeQpcuYqyJjn-9aEMGTzujlMrMBvGuvzhtss'});
+    const [Picture,setPicture] = React.useState('nonPicture');
 
     const userStore = {
         Token : Token,
-        updateToken : (Token)=> setToken(Token)
+        Picture: Picture,
+        updateToken : (Token)=> setToken(Token),
+        updatePicture:(Picture)=> setPicture(Picture)
+        
     }
-
+    // const deToken = jwt_decode(userStore.Token);
+    // setPicture = deToken.image;
+    
     return(
         <UserStoreContext.Provider value = {userStore}>
             {children}
